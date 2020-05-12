fecbe10146ac44fb5afb60a29c8d4d552a85797e
JAIMIN GANDHI <jgandhi8@gmail.com>
2020-05-11 21:46:24 -0400

    Adding update and flash message

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'blog', to: 'pages#blog'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles
 end
